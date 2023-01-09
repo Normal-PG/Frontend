eae7635a13c2367135cd2caf8bba12944d8d4200
SubaRya <index20010928@gmail.com>
2023-01-10 04:25:08 +0800

    feat: add auto import & vue-component auto import plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import Components from 'unplugin-vue-components/vite'
+import { resolve } from "path"
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+import AutoImport from "unplugin-auto-import/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-});
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [AntDesignVueResolver()],
+    }),
+    AutoImport({
+      imports: ["vue", "vue-router"],
+    }),
+  ],
+})
